969378eefbd34ec59b289d46a4eec14eb76c5391
Madhu Garapati <mk1240598@gmail.com>
2026-04-07 11:05:53 +0530

    deployment code ready

--- mlfp/app.py
+++ mlfp/app.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
 import joblib
 import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 model = joblib.load("model.pkl")
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def home():
-    return {"message": "ML API Running 🚀"}
+    with open("mlfp/index.html", encoding="utf-8") as f:
+        return f.read()
 
 @app.post("/predict")
 def predict(data: dict):
